1af6d3061f949975b70745a1e1bf76e0f36543e9
Ivan Sharov <ivan.sharov@sfera.zone>
2025-04-28 21:20:10 +0300

    Добавлен файл requirements.txt и addEditCoffeeForm.ui. Добавлен функционал для кнопок изменения и добавления

--- main.py
+++ main.py
@@ -7,8 +7,49 @@ conn = sqlite3.connect('coffee.sqlite')
 cursor = conn.cursor()
 
 
-class AddEditCoffeeForm:
-    pass
+class AddEditCoffeeForm(QtWidgets.QDialog):
+    
+    def __init__(self, coffee_id=None):
+        super(AddEditCoffeeForm, self).__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+
+        # Если передан ID, загружаем данные для редактирования
+        self.coffee_id = coffee_id
+        if coffee_id:
+            cursor.execute("SELECT * FROM coffee WHERE id = ?", (coffee_id,))
+            row = cursor.fetchone()
+            self.lineEditName.setText(row[1])
+            self.comboBoxRoastLevel.setCurrentText(row[2])
+            self.comboBoxType.setCurrentText(row[3])
+            self.lineEditTasteDescription.setText(row[4])
+            self.doubleSpinBoxPrice.setValue(row[5])
+            self.spinBoxPackageVolume.setValue(row[6])
+
+        # Настройка кнопки Save
+        self.pushButtonSave.clicked.connect(self.save_coffee)
+        
+    def save_coffee(self):
+        name = self.lineEditName.text()
+        roast_level = self.comboBoxRoastLevel.currentText()
+        type_ = self.comboBoxType.currentText()
+        taste_description = self.lineEditTasteDescription.text()
+        price = self.doubleSpinBoxPrice.value()
+        package_volume = self.spinBoxPackageVolume.value()
+
+        if self.coffee_id:
+            cursor.execute("""
+                UPDATE coffee SET name=?, roast_level=?, type=?, taste_description=?, price=?, package_volume=?
+                WHERE id=?
+            """, (name, roast_level, type_, taste_description, price, package_volume, self.coffee_id))
+        else:
+            cursor.execute("""
+                INSERT INTO coffee (name, roast_level, type, taste_description, price, package_volume)
+                VALUES (?, ?, ?, ?, ?, ?)
+            """, (name, roast_level, type_, taste_description, price, package_volume))
+
+        conn.commit()
+        self.accept()
+        
 
 class CoffeeApp(QtWidgets.QMainWindow):
     def __init__(self):
